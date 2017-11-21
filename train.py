09b292a062a78f63665b1ac8b73ea2613439bc51
lizeqian <lizeqian@gmail.com>
2017-11-21 11:16:27 -0500

    load data

--- train.py
+++ train.py
@@ -11,19 +11,33 @@ import os
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import RandomSampler
-from model.network import Net
 import torch.optim as optim
 from logger import Logger
 
 class Rand_num(Dataset):
     def __init__(self):   
-        data = []
-        label = []
+        self.dirs = []
+        self.label = []
         for i in range(3):
-            data.append(sorted(os.listdir("data/dataset/"+str(i))))
-            length = len(data[i])
-            label.append(np.ones(length)*i)
-        label = np.concatenate(label)    
+            self.dirs.append(sorted(os.listdir("data/dataset/"+str(i))))
+            length = len(self.dirs[i])
+            self.label.append(np.ones(length)*i)
+        for i in range(3):
+            for j in range(len(self.dirs[i])):
+                self.dirs[i][j] = os.path.join("data/dataset/"+str(i),self.dirs[i][j])
+        
+        self.data = []
+        
+        for i in range(1):
+            for j in range(1):
+                sample = []
+                for dir in sorted(os.listdir(self.dirs[i][j])):
+                    dirs_mod = os.path.join(self.dirs[i][j],dir)
+                    print(dirs_mod)
+                    sample.append(cv2.imread(dirs_mod,1))
+                self.data.append(sample)
+                
+        self.label = np.concatenate(self.label)    
             
         self.classes=('0', '1', '2')
 
@@ -40,3 +54,7 @@ class Rand_num(Dataset):
     def __len__(self):
 #        print ('\tcalling Dataset:__len__')
         return len(self.images)
+    
+if __name__ == '__main__':
+    dataset = Rand_num();
+    a=dataset.data
