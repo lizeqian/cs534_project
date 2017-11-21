e517f6d598de9b443b27aa423215e9f29c9cac24
lizeqian <lizeqian@gmail.com>
2017-11-21 18:49:00 -0500

    complete dataset

--- train.py
+++ train.py
@@ -16,45 +16,29 @@ from logger import Logger
 
 class Rand_num(Dataset):
     def __init__(self):   
-        self.dirs = []
+        dirs = []
         self.label = []
         for i in range(3):
-            self.dirs.append(sorted(os.listdir("data/dataset/"+str(i))))
-            length = len(self.dirs[i])
+            dirs.append(sorted(os.listdir("data/dataset/"+str(i))))
+            length = len(dirs[i])
             self.label.append(np.ones(length)*i)
         for i in range(3):
-            for j in range(len(self.dirs[i])):
-                self.dirs[i][j] = os.path.join("data/dataset/"+str(i),self.dirs[i][j])
-        
-        self.data = []
-        
-        for i in range(1):
-            for j in range(1):
-                sample = []
-                for dir in sorted(os.listdir(self.dirs[i][j])):
-                    dirs_mod = os.path.join(self.dirs[i][j],dir)
-                    print(dirs_mod)
-                    sample.append(cv2.imread(dirs_mod,1))
-                self.data.append(sample)
-                
-        self.label = np.concatenate(self.label)    
-            
-        self.classes=('0', '1', '2')
+            for j in range(len(dirs[i])):
+                dirs[i][j] = os.path.join("data/dataset/"+str(i),dirs[i][j])
+        self.directories = np.concatenate((dirs[0],dirs[1],dirs[2]))
+        self.label = np.concatenate((self.label[0], self.label[1], self.label[2]))
+        assert len(self.directories) == len(self.label)
 
     def __getitem__(self, index):
-        #print ('\tcalling Dataset:__getitem__ @ idx=%d'%index)
-        img = self.images[index]
-        label = self.labels[index]
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        return img, label
+        dirs = self.directories[index]
+        data = []
+        for element in sorted(os.listdir(dirs)):
+            dirs_mod = os.path.join(dirs,element)
+            data.append(cv2.imread(dirs_mod,1))
+        return data, self.label[index]
 
     def __len__(self):
-#        print ('\tcalling Dataset:__len__')
-        return len(self.images)
+        return len(self.directories)
     
 if __name__ == '__main__':
     dataset = Rand_num();
-    a=dataset.data
