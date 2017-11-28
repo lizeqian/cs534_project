bb66faddde1f510d077a8feebdb2c74a7b1f4476
lizeqian <lizeqian@gmail.com>
2017-11-27 22:02:15 -0500

    add pretrained network

---
+++ alexnet.py
@@ -0,0 +1,112 @@
+import torch.nn as nn
+import torch
+import torch.utils.model_zoo as model_zoo
+from torch.utils.data.dataset import Dataset
+import datetime
+import cv2
+import sys
+import os
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import RandomSampler
+from torch.autograd import Variable
+
+__all__ = ['AlexNet', 'alexnet']
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+class Rand_num(Dataset):
+    def __init__(self):
+        dirs = []
+        self.label = []
+        for i in range(3):
+            dirs.append(sorted(os.listdir("data/dataset/"+str(i))))
+            length = len(dirs[i])
+            self.label.append(np.ones(length)*i)
+        for i in range(3):
+            for j in range(len(dirs[i])):
+                dirs[i][j] = os.path.join("data/dataset/"+str(i),dirs[i][j])
+        self.directories = np.concatenate((dirs[0],dirs[1],dirs[2]))
+        self.label = np.concatenate((self.label[0], self.label[1], self.label[2]))
+        assert len(self.directories) == len(self.label)
+
+    def __getitem__(self, index):
+        dirs = self.directories[index]
+        data = []
+        for element in sorted(os.listdir(dirs)):
+            dirs_mod = os.path.join(dirs,element)
+            img=cv2.imread(dirs_mod,1)
+            img=cv2.resize(img,None,fx=227.0/480, fy=227.0/270, interpolation = cv2.INTER_CUBIC)
+            data.append(np.swapaxes(np.swapaxes(img, 2, 1), 1, 0))
+        return np.array(data), np.ones(len(data))*self.label[index]
+
+    def __len__(self):
+        return len(self.directories)
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
+def alexnet(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = AlexNet(**kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
+    return model
+
+if __name__ == '__main__':
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    torch.backends.cudnn.benchmark = True
+    net = alexnet(pretrained = True)
+    net.cuda()
+    dataset = Rand_num()
+    sampler = RandomSampler(dataset)
+    loader = DataLoader(dataset, batch_size = 1, sampler = sampler, shuffle = False, num_workers=2)
+    
+    for i, data in enumerate(loader, 0):
+        video, labels = data
+        labels = torch.squeeze(Variable(labels.long().cuda()))
+        video = torch.squeeze(Variable((video.float()/256).cuda()))
+        net.train()
+        outputs = net.forward(video)
+        print (outputs)
+        break
