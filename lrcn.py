774f6e88a198322c7694b251f1ec29957ecf1108
lizeqian <lizeqian@gmail.com>
2017-11-28 23:30:13 -0500

    add lrcn

---
+++ lrcn.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import torch.autograd as autograd
+
+class LRCN(nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, lstm_layer, batch):
+        super(LRCN, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.batch = batch
+        self.num_layers = lstm_layer
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, lstm_layer)
+        self.hidden = self.init_hidden()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, embedding_dim),
+        )
+        
+    def init_hidden(self):
+        return (autograd.Variable(torch.zeros(self.num_layers, self.batch, self.hidden_dim)),
+                autograd.Variable(torch.zeros(self.num_layers, self.batch, self.hidden_dim)))
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        print(x.size())
+        lstm_out, self.hidden = self.lstm(x, self.hidden)
+        outs = lstm_out[-1]
+        outs = F.log_softmax(outs)
+        return outs
+
+
+
+
+
---
+++ trainlrcn.py
@@ -0,0 +1,74 @@
+import numpy as np
+import torch
+from torchvision import models, transforms
+from torch.autograd import Variable
+import datetime
+import cv2
+import sys
+import os
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import RandomSampler
+import torch.optim as optim
+from logger import Logger
+from lrcn import LRCN
+
+class Rand_num(Dataset):
+    def __init__(self):
+        dirs = []
+        self.label = []
+        for i in range(3):
+            dirs.append(sorted(os.listdir("data/dataset/"+str(i))))
+            length = len(dirs[i])
+            self.label.append(np.ones(length)*i)
+        for i in range(3):
+            for j in range(len(dirs[i])):
+                dirs[i][j] = os.path.join("data/dataset/"+str(i),dirs[i][j])
+        self.directories = np.concatenate((dirs[0],dirs[1],dirs[2]))
+        self.label = np.concatenate((self.label[0], self.label[1], self.label[2]))
+        assert len(self.directories) == len(self.label)
+
+    def __getitem__(self, index):
+        dirs = self.directories[index]
+        img_dir = sorted(os.listdir(dirs))
+        data = []
+        for i in range(50):
+            dirs_mod = os.path.join(dirs,img_dir[i])
+            img=cv2.imread(dirs_mod,1)
+            img=cv2.resize(img,None,fx=227.0/480, fy=227.0/270, interpolation = cv2.INTER_CUBIC)
+            data.append(np.swapaxes(np.swapaxes(img, 2, 1), 1, 0))
+        return np.array(data), self.label[index]
+
+    def __len__(self):
+        return len(self.directories)
+
+if __name__ == '__main__':
+    #####Please comment out the following 2 lines for cpu use################
+#    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+#    torch.backends.cudnn.benchmark = True
+    batch_size = 20
+    SAVE_PATH = './cp.bin'
+    
+    dataset = Rand_num()
+    sampler = RandomSampler(dataset)
+    loader = DataLoader(dataset, batch_size, sampler = sampler, shuffle = False, num_workers=1, drop_last=True)
+    net = LRCN(256, 3, 5, batch_size)
+#    net.cuda()
+    optimizer = optim.Adam(net.parameters(), lr=0.00001)
+    for epoch in range(10000):
+        for i, data in enumerate(loader, 0):
+            video, labels = data
+            video = video.view(-1,3,227,227)
+            net.zero_grad()
+            net.hidden = net.init_hidden()
+            labels = Variable(labels.long())
+            video = Variable((video.float()/256))
+            net.train()
+            outputs = net.forward(video)
+            loss = net.lossFunction(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            if i == 0:
+                torch.save(net.state_dict(), SAVE_PATH)
+                print (loss)
+
