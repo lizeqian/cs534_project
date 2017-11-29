fix bug in softmax

lstmlayer.py
@@ -11,6 +11,7 @@ class LSTMLayer(nn.Module):
         self.num_layers = lstm_layer
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, lstm_layer)
         self.hidden = self.init_hidden()
+        self.sm = nn.Softmax()
 
     def init_hidden(self):
         return (autograd.Variable(torch.zeros(self.num_layers, self.batch, self.hidden_dim)),
@@ -20,5 +21,5 @@ class LSTMLayer(nn.Module):
 #        inputs = inputs.contiguous().view(inputs.size()[0], self.batch, inputs.size()[1])
         lstm_out, self.hidden = self.lstm(inputs, self.hidden)
         outs = lstm_out[-1]
-        outs = F.log_softmax(outs)
+        outs = self.sm(outs)
         return outs