1d0eaed3e5838beecdafe3a3477ee2b00757f38d
lizeqian <lizeqian@gmail.com>
2017-11-30 16:43:25 -0500

    lstmtest

--- testlstm.py
+++ testlstm.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
     torch.backends.cudnn.benchmark = True
 
-    SAVE_PATH = './cp_lstm.bin'
+    SAVE_PATH = './cp_lstm1.bin'
 
     lossfunction = nn.CrossEntropyLoss()
     batch_size = 150
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     dataset = Rand_num()
     sampler = RandomSampler(dataset)
     loader = DataLoader(dataset, batch_size, sampler = sampler, shuffle = False, num_workers=1, drop_last=True)
-    net = LSTMLayer(1000, 3, 5, batch_size)
+    net = LSTMLayer(1000, 1024, 5, batch_size)
     net.load_state_dict(torch.load(SAVE_PATH))
     net.cuda()
     for i, data in enumerate(loader, 0):
