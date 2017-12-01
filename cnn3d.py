1983b82c6c9dfa16ccdc30d8a3844a51640951c2
Zeqian <zli14@turing.cm.cluster>
2017-11-30 23:40:52 -0500

    add 3dcnn

---
+++ cnn3d.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+import torch
+import torch.utils.model_zoo as model_zoo
+from torch.utils.data.dataset import Dataset
+import datetime
+import cv2
+import sys
+import os
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import RandomSampler
+from torch.autograd import Variable
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv3d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=3, stride=2),
+            nn.Conv3d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv3d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
